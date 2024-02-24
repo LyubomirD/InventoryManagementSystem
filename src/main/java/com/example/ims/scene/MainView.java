78419b573eabbb19a8f02a310e7189f992775f73
Lyubo <lubodimoff@gmail.com>
2024-02-24 19:55:05 +0200

    Change view from main-view to product-view

--- src/main/java/com/example/ims/scene/MainView.java
+++ src/main/java/com/example/ims/scene/MainView.java
@@ -1,6 +1,30 @@
 package com.example.ims.scene;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class MainView {
 
+    @FXML
+    private StackPane contentPane;
 
+    private boolean productViewLoaded = false;
+
+    @FXML
+    private void loadProductScene() {
+        if (!productViewLoaded) {
+            try {
+                AnchorPane productView = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/ims/product-view.fxml")));
+                contentPane.getChildren().setAll(productView);
+                productViewLoaded = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
