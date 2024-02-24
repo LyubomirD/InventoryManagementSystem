56cfd15ee1b999cf71f61de91201e798f323ba07
Lyubo <lubodimoff@gmail.com>
2024-02-24 20:53:02 +0200

    Welcome scene, and I can view the other scenes

--- src/main/java/com/example/ims/scene/MainView.java
+++ src/main/java/com/example/ims/scene/MainView.java
@@ -4,7 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -14,14 +17,11 @@ public class MainView {
     @FXML
     private StackPane contentPane;
 
-    private boolean viewLoaded = false;
-
     @FXML
     private void loadScene(String fxmlPath) {
         try {
             AnchorPane view = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxmlPath)));
             contentPane.getChildren().setAll(view);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
